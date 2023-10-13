e9f48fa1d23c217348a61d331834c0cf29dc64e8
vinh <thanhvinhht@outlook.com>
2023-10-13 21:40:21 +0700

    Edit file Program

--- P1/Program.cs
+++ P1/Program.cs
@@ -15,6 +15,7 @@ namespace P1
         [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new QuanlyCT_TienNghi());
